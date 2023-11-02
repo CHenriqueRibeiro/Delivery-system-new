300b213624d372bca82dfacc0937f38b930fc705
Ezequiel Brilhante <ezequielrb7@gmail.com>
2023-11-02 17:39:08 -0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,39 +1,47 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import RegisterScreen from "./pages/RegisterScreen.jsx";
-import Order from "./pages/Order.jsx";
-import OrdersWithoutRegistration from "./pages/OrdersWithoutRegistration.jsx";
+import {
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
+import Home from './pages/Home.jsx';
+import RegisterScreen from './pages/RegisterScreen.jsx';
+import Order from './pages/Order.jsx';
+import OrdersWithoutRegistration from './pages/OrdersWithoutRegistration.jsx';
+// import { Admin } from '../../material-kit-react/src/main.jsx';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Home />,
       },
       {
-        path: "/cadastro",
+        path: '/cadastro',
         element: <RegisterScreen />,
       },
       {
-        path: "/pedido",
+        path: '/pedido',
         element: <Order />,
       },
       {
-        path: "/pedidosemcadastro",
+        path: '/pedidosemcadastro',
         element: <OrdersWithoutRegistration />,
       },
+      // {
+      //   path: '/admin',
+      //   element: <Admin />,
+      // },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
